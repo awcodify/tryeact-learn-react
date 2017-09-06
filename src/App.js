8bd71ecf42e5414cc47dd30264d91313ca23df80
Danu Akbar <awcodify@gmail.com>
2017-09-06 15:27:25 +0700

    enable filtering todos by it's status

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import logo from './logo.svg'
 import './App.css';
 import { TodoForm, TodoList, Footer } from './components/todo'
-import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo } from './lib/todoHelpers'
+import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos } from './lib/todoHelpers'
 import { pipe, partial } from './lib/utils'
 
 class App extends Component {
@@ -13,6 +14,10 @@ class App extends Component {
       { id: 2, name: 'And this our last item', isCompleted: false }
     ],
     currentTodo: ''
+  }
+
+  static contextTypes = {
+    route: PropTypes.string
   }
 
   handleToggle = (id) => {
@@ -55,6 +60,7 @@ class App extends Component {
 
   render() {
     const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
+    const displayTodos = filterTodos(this.state.todos, this.context.route)
     return (
       <div className="App">
         <div className="App-header">
@@ -67,7 +73,7 @@ class App extends Component {
           handleSubmit={submitHandler}
           currentTodo={this.state.currentTodo}/>
           <TodoList handleToggle={this.handleToggle}
-            todos={this.state.todos}
+            todos={displayTodos}
             handleRemove={this.handleRemove} />
           <Footer />
         </div>
--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo, findById, toggleTodo, updateTodo, removeTodo} from './todoHelpers'
+import {addTodo, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './todoHelpers'
 
 test('addTodo should add the passed todo to the list', () => {
     const startTodos = [
@@ -97,4 +97,41 @@ test('removeTodo should not mutate the original array', () => {
 
     const result = removeTodo(startTodos, targetId);
     expect(result).not.toBe(expectedTodos)
+})
+
+test('filterTodos should return all items for the root route', () => {
+    const startTodos = [
+        { id: 0, name: 'First todo', isCompleted: false },
+        { id: 1, name: 'Second todo', isCompleted: false },
+        { id: 2, name: 'This our new todo', isCompleted: false }
+    ]
+    const result = filterTodos(startTodos, '/')
+    expect(result).toEqual(startTodos)
+})
+
+test('filterTodos should return only completed items for the complete route', ()=> {
+    const startTodos = [
+        { id: 0, name: 'First todo', isCompleted: true },
+        { id: 1, name: 'Second todo', isCompleted: false },
+        { id: 2, name: 'This our new todo', isCompleted: false }
+    ]
+    const expectedTodos = [
+        { id: 0, name: 'First todo', isCompleted: true }
+    ]
+    const result = filterTodos(startTodos, '/')
+    expect(result).toEqual(startTodos)
+})
+
+test('filterTodos should return only completed items for the active route', ()=> {
+    const startTodos = [
+        { id: 0, name: 'First todo', isCompleted: true },
+        { id: 1, name: 'Second todo', isCompleted: false },
+        { id: 2, name: 'This our new todo', isCompleted: false }
+    ]
+    const expectedTodos = [
+         { id: 1, name: 'Second todo', isCompleted: false },
+        { id: 2, name: 'This our new todo', isCompleted: false }
+    ]
+    const result = filterTodos(startTodos, '/')
+    expect(result).toEqual(startTodos)
 })
