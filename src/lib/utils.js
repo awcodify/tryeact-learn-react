89f497b6a17120465f07302e3e7a775e69b3a72d
Danu Akbar <awcodify@gmail.com>
2017-09-06 13:36:01 +0700

    create a pipe function for handling function composition

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,3 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args)
+const _pipe = (fn, fn1) => (...args) => fn1(fn(...args)) 
+export const pipe = (...fns) => fns.reduce(_pipe);
--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -1,8 +1,9 @@
-import { partial } from './utils'
+import { partial, pipe } from './utils'
 
 const add = (a, b) => a + b
-
 const multipleAdd = (a, b, c) => a + b + c
+const increment = (number) => number + 1
+const double = (number) => number * 2
 
 test('partial applied the first argument ahead of time', () => {
   const inc = partial(add, 1)
@@ -14,4 +15,22 @@ test('partial applies the multiple arguments ahead of time', () => {
   const inc = partial(multipleAdd, 1, 3)
   const result = inc(2)
   expect(result).toBe(6)
+})
+
+test('pipe passes the results of increment to double', () => {
+    const pipeline = pipe(increment, double);
+    const result = pipeline(2)
+    expect(result).toBe(6)
+})
+
+test('pipe passes the results of double to increment', () => {
+    const pipeline = pipe(double, increment)
+    const result = pipeline(2)
+    expect(result).toBe(5)
+})
+
+test('pipe works with more than 2 functions', () => {
+    const pipeline = pipe(add, increment, double, increment)
+    const result = pipeline(1,2)
+    expect(result).toBe(9)
 })
